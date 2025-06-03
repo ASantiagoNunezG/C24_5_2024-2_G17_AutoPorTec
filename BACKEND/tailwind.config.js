09b3a857b3c9c5a21db83a34ae6f33f268536b2b
A. Santiago Nunez <abraham.nunez@tecsup.edu.pe>
2025-06-03 07:57:03 -0500

    Flujo y diseño terminado con exito

---
+++ BACKEND/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'class',  // usa modo oscuro basado en clase
+  content: [
+    './templates/**/*.html',  // ajusta según la ruta donde tienes tus templates
+    './static/js/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
